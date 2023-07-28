831b875c9a8c683350d2b797c0e0c5c240295143
Shivam Singh <singhshivamsunil@gmail.com>
2023-07-28 10:52:22 +0530

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the http module from Node.js
+const http = require('http');
+
+// Define the port you want to listen to
+const PORT = 3000;
+
+// Create a server that responds to all requests with "Hello, World!"
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
