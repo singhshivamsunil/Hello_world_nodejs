9500ff5524e5d7b22960e97abe7e0bb07546f6db
Shivam Singh <singhshivamsunil@gmail.com>
2023-07-28 10:57:15 +0530

    name change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello World by shivam!\n');
 });
 
 // Start the server
